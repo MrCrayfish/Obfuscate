fa0ab047b3901c0b5a512faa6c3a78ba7713a988
MrCrayfish <mrcrayfish@hotmail.com>
2019-09-10 21:10:41 +0930

    Added missing parameters to player model events

--- src/main/java/com/mrcrayfish/obfuscate/client/event/PlayerModelEvent.java
+++ src/main/java/com/mrcrayfish/obfuscate/client/event/PlayerModelEvent.java
@@ -11,18 +11,60 @@ import net.minecraftforge.eventbus.api.Cancelable;
 public abstract class PlayerModelEvent extends PlayerEvent
 {
     private PlayerModel modelPlayer;
+    private float limbSwing;
+    private float limbSwingAmount;
+    private float ageInTicks;
+    private float netHeadYaw;
+    private float headPitch;
+    private float scale;
     private float partialTicks;
 
-    private PlayerModelEvent(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+    private PlayerModelEvent(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
     {
         super(player);
         this.modelPlayer = modelPlayer;
+        this.limbSwing = limbSwing;
+        this.limbSwingAmount = limbSwingAmount;
+        this.ageInTicks = ageInTicks;
+        this.netHeadYaw = netHeadYaw;
+        this.headPitch = headPitch;
+        this.scale = scale;
         this.partialTicks = partialTicks;
     }
 
     public PlayerModel getModelPlayer()
     {
         return modelPlayer;
+    }
+
+    public float getLimbSwing()
+    {
+        return limbSwing;
+    }
+
+    public float getLimbSwingAmount()
+    {
+        return limbSwingAmount;
+    }
+
+    public float getAgeInTicks()
+    {
+        return ageInTicks;
+    }
+
+    public float getNetHeadYaw()
+    {
+        return netHeadYaw;
+    }
+
+    public float getHeadPitch()
+    {
+        return headPitch;
+    }
+
+    public float getScale()
+    {
+        return scale;
     }
 
     public float getPartialTicks()
@@ -33,24 +75,24 @@ public abstract class PlayerModelEvent extends PlayerEvent
     @Cancelable
     public static class SetupAngles extends PlayerModelEvent
     {
-        private SetupAngles(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+        private SetupAngles(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
         {
-            super(player, modelPlayer, partialTicks);
+            super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
         }
 
         public static class Pre extends SetupAngles
         {
-            public Pre(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+            public Pre(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
             {
-                super(player, modelPlayer, partialTicks);
+                super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
             }
         }
 
         public static class Post extends SetupAngles
         {
-            public Post(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+            public Post(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
             {
-                super(player, modelPlayer, partialTicks);
+                super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
             }
 
             @Override
@@ -64,24 +106,24 @@ public abstract class PlayerModelEvent extends PlayerEvent
     @Cancelable
     public static class Render extends PlayerModelEvent
     {
-        private Render(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+        private Render(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
         {
-            super(player, modelPlayer, partialTicks);
+            super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
         }
 
         public static class Pre extends Render
         {
-            public Pre(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+            public Pre(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
             {
-                super(player, modelPlayer, partialTicks);
+                super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
             }
         }
 
         public static class Post extends Render
         {
-            public Post(PlayerEntity player, PlayerModel modelPlayer, float partialTicks)
+            public Post(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale, float partialTicks)
             {
-                super(player, modelPlayer, partialTicks);
+                super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
             }
 
             @Override
