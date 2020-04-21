e7a3389c5830491ae47cb75a5f41696748b247f9
MrCrayfish <mrcrayfish@hotmail.com>
2020-04-21 23:59:26 +0930

    Fixed digest error on load

--- src/main/java/com/mrcrayfish/obfuscate/client/Hooks.java
+++ src/main/java/com/mrcrayfish/obfuscate/client/Hooks.java
@@ -8,6 +8,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -20,12 +21,12 @@ public class Hooks
 {
     public static boolean fireRenderGuiItemPre(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer)
     {
-        return MinecraftForge.EVENT_BUS.post(new RenderItemEvent.Gui.Pre(heldItem, matrixStack, renderTypeBuffer));
+        return MinecraftForge.EVENT_BUS.post(new RenderItemEvent.Gui.Pre(heldItem, matrixStack, renderTypeBuffer, 15728880, OverlayTexture.DEFAULT_LIGHT));
     }
 
     public static void fireRenderGuiItemPost(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer)
     {
-        MinecraftForge.EVENT_BUS.post(new RenderItemEvent.Gui.Post(heldItem, matrixStack, renderTypeBuffer));
+        MinecraftForge.EVENT_BUS.post(new RenderItemEvent.Gui.Post(heldItem, matrixStack, renderTypeBuffer, 15728880, OverlayTexture.DEFAULT_LIGHT));
     }
 
     public static boolean fireRenderPlayerPre(LivingEntity entity, EntityModel model, MatrixStack matrixStack, IVertexBuilder builder, int light, int overlay, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch)
--- src/main/java/com/mrcrayfish/obfuscate/client/event/RenderItemEvent.java
+++ src/main/java/com/mrcrayfish/obfuscate/client/event/RenderItemEvent.java
@@ -3,7 +3,6 @@ package com.mrcrayfish.obfuscate.client.event;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
@@ -158,24 +157,24 @@ public class RenderItemEvent extends Event
     @Cancelable
     public static class Gui extends RenderItemEvent
     {
-        public Gui(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer)
+        public Gui(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
         {
-            super(heldItem, ItemCameraTransforms.TransformType.GUI, matrixStack, renderTypeBuffer, 15728880, OverlayTexture.DEFAULT_LIGHT, 0.0F);
+            super(heldItem, ItemCameraTransforms.TransformType.GUI, matrixStack, renderTypeBuffer, light, overlay, 0.0F);
         }
 
         public static class Pre extends Gui
         {
-            public Pre(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer)
+            public Pre(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
             {
-                super(heldItem, matrixStack, renderTypeBuffer);
+                super(heldItem, matrixStack, renderTypeBuffer, light, overlay);
             }
         }
 
         public static class Post extends Gui
         {
-            public Post(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer)
+            public Post(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
             {
-                super(heldItem, matrixStack, renderTypeBuffer);
+                super(heldItem, matrixStack, renderTypeBuffer, light, overlay);
             }
 
             @Override
