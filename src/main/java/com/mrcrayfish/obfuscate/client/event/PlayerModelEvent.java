3ede452352aa26dc795703c540202e9fbeb45bb7
MrCrayfish <mrcrayfish@hotmail.com>
2021-01-15 23:57:27 +1030

    Added documentation to events 🙃 (I don't like writing docs)

--- src/main/java/com/mrcrayfish/obfuscate/client/event/PlayerModelEvent.java
+++ src/main/java/com/mrcrayfish/obfuscate/client/event/PlayerModelEvent.java
@@ -8,6 +8,8 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.Cancelable;
 
 /**
+ * The base class for player model events.
+ *
  * Author: MrCrayfish
  */
 public abstract class PlayerModelEvent extends PlayerEvent
@@ -32,41 +34,68 @@ public abstract class PlayerModelEvent extends PlayerEvent
         this.partialTicks = partialTicks;
     }
 
+    /**
+     * Gets an instance of the player model
+     */
     public PlayerModel getModelPlayer()
     {
         return this.modelPlayer;
     }
 
+    /**
+     * Gets the current limb swing
+     */
     public float getLimbSwing()
     {
         return this.limbSwing;
     }
 
+    /**
+     * Gets the current limb swing amount
+     */
     public float getLimbSwingAmount()
     {
         return this.limbSwingAmount;
     }
 
+    /**
+     * Gets the current age of the player (includes partial ticks)
+     */
     public float getAgeInTicks()
     {
         return this.ageInTicks;
     }
 
+    /**
+     * Gets the current yaw of the player
+     */
     public float getNetHeadYaw()
     {
         return this.netHeadYaw;
     }
 
+    /**
+     * Gets the current pitch of the player
+     */
     public float getHeadPitch()
     {
         return this.headPitch;
     }
 
+    /**
+     * Gets the partial ticks of the current render
+     */
     public float getPartialTicks()
     {
         return this.partialTicks;
     }
 
+    /**
+     * This event is fired when setting up the rotations of the player's limbs. This can be used to
+     * change the pose of the player. A few uses of this event may be to change the pose when holding
+     * a certain item or creating a more accurate pose of the player when riding an specific entity.
+     * Use {@link Pre} or {@link Post} when subscribing to this event.
+     */
     @Cancelable
     public static class SetupAngles extends PlayerModelEvent
     {
@@ -75,6 +104,13 @@ public abstract class PlayerModelEvent extends PlayerEvent
             super(player, modelPlayer, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, partialTicks);
         }
 
+        /**
+         * Fired before rotations are apply to the players limbs. This can be cancelled to prevent
+         * Minecraft from applying rotations to the player and adding your own custom rotations.
+         * It should be noted that changing the rotations of the players limbs and not cancelling
+         * the event will result in the changes being overridden. Consider using {@link Post} to
+         * modify the rotations if you aren't doing a complete custom implementation.
+         */
         public static class Pre extends SetupAngles
         {
             public Pre(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float partialTicks)
@@ -83,6 +119,13 @@ public abstract class PlayerModelEvent extends PlayerEvent
             }
         }
 
+        /**
+         * Fired after rotations have been applied to the player model. This will only be called if
+         * {@link Pre} was not cancelled. The benefit of using this over {@link Pre} is that
+         * animations such as the player's legs walking will still be included. This event is
+         * appropriate for most uses, such as creating a custom pose if holding a certain item. This
+         * event cannot be cancelled.
+         */
         public static class Post extends SetupAngles
         {
             public Post(PlayerEntity player, PlayerModel modelPlayer, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float partialTicks)
@@ -98,6 +141,10 @@ public abstract class PlayerModelEvent extends PlayerEvent
         }
     }
 
+    /**
+     * This event is fired when rendering the player. Unlike {@link net.minecraftforge.client.event.RenderPlayerEvent}
+     * from Forge, this event includes transformations applied before rendering the model.
+     */
     @Cancelable
     public static class Render extends PlayerModelEvent
     {
@@ -115,6 +162,11 @@ public abstract class PlayerModelEvent extends PlayerEvent
             this.overlay = overlay;
         }
 
+        /**
+         * Fired just before the player model is rendered. It should be noted that cancelling this
+         * event will prevent {@link Post} from being fired. Cancelling this event will prevent the
+         * player model from being rendered.
+         */
         public static class Pre extends Render
         {
             public Pre(PlayerEntity player, PlayerModel modelPlayer, MatrixStack matrixStack, IVertexBuilder builder, int light, int overlay, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float partialTicks)
@@ -123,6 +175,11 @@ public abstract class PlayerModelEvent extends PlayerEvent
             }
         }
 
+        /**
+         * Called after the player model has been rendered. This event is good for any additional
+         * rendering you want to apply to the player. This will only be called if {@link Pre} was
+         * not cancelled.
+         */
         public static class Post extends Render
         {
             public Post(PlayerEntity player, PlayerModel modelPlayer, MatrixStack matrixStack, IVertexBuilder builder, int light, int overlay, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float partialTicks)
@@ -137,21 +194,33 @@ public abstract class PlayerModelEvent extends PlayerEvent
             }
         }
 
+        /**
+         * Gets the current matrix stack
+         */
         public MatrixStack getMatrixStack()
         {
             return this.matrixStack;
         }
 
+        /**
+         * Gets the vertex builder for building entity models
+         */
         public IVertexBuilder getBuilder()
         {
             return this.builder;
         }
 
+        /**
+         * Gets the current light of the player model
+         */
         public int getLight()
         {
             return this.light;
         }
 
+        /**
+         * Gets the overlay texture
+         */
         public int getOverlay()
         {
             return this.overlay;
--- src/main/java/com/mrcrayfish/obfuscate/client/event/RenderItemEvent.java
+++ src/main/java/com/mrcrayfish/obfuscate/client/event/RenderItemEvent.java
@@ -12,7 +12,24 @@ import net.minecraft.util.HandSide;
 import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
+import java.util.function.Supplier;
+
 /**
+ * The base class for item rendering events. It is possible to subscribe to this event to capture
+ * all rendering events.
+ * <p><br>
+ * Although it's possible to control rendering of an item using
+ * {@link net.minecraft.item.Item.Properties#setISTER(Supplier)}, the location of the render call is
+ * not guaranteed. These events are hooked into the code at the exact rendering calls rather than
+ * relying on the camera transform type.
+ * <p><br>
+ * The current available events are:<br>
+ * {@link Held} - called when rendering held item on the player's model<br>
+ * {@link Entity} - called when rendering an item on the floor<br>
+ * {@link Gui} - called when rendering an item in a GUI<br>
+ * {@link ItemFrame} - called when rendering an item in an item frame<br>
+ * {@link Head} - called when rendering an item on a player's head (item is in head slot)<br>
+ * <p><br>
  * Author: MrCrayfish
  */
 @Cancelable
@@ -37,41 +54,67 @@ public class RenderItemEvent extends Event
         this.overlay = overlay;
     }
 
+    /**
+     * Gets the current stack to be rendered
+     */
     public ItemStack getItem()
     {
         return this.heldItem;
     }
 
+    /**
+     * Gets the transform type of the render
+     */
     public ItemCameraTransforms.TransformType getTransformType()
     {
         return this.transformType;
     }
 
+    /**
+     * Gets the current matrix stack
+     */
     public MatrixStack getMatrixStack()
     {
         return this.matrixStack;
     }
 
+    /**
+     * Gets an instance of the render type buffer
+     */
     public IRenderTypeBuffer getRenderTypeBuffer()
     {
         return this.renderTypeBuffer;
     }
 
+    /**
+     * Gets the light for the current render
+     */
     public int getLight()
     {
         return this.light;
     }
 
+    /**
+     * Gets the overlay texture for the render
+     */
     public int getOverlay()
     {
         return this.overlay;
     }
 
+    /**
+     * Gets the current partial ticks
+     */
     public float getPartialTicks()
     {
         return this.partialTicks;
     }
 
+    /**
+     * This event is fired when rendering the held item on the player's model. This is only called
+     * when looking at other players or when the current point of view is third person. Use
+     * {@link Pre} or {@link Post} when subscribing to this event.
+     */
     @Cancelable
     public static class Held extends RenderItemEvent
     {
@@ -85,16 +128,27 @@ public class RenderItemEvent extends Event
             this.handSide = handSide;
         }
 
+        /**
+         * Gets the entity holding the item
+         */
         public LivingEntity getEntity()
         {
             return this.entity;
         }
 
+        /**
+         * Gets the hand side that is holding the item
+         */
         public HandSide getHandSide()
         {
             return this.handSide;
         }
 
+        /**
+         * Called before the item is rendered in the players hand. This event can be cancelled to
+         * prevent the item from being rendered and perform any custom rendering. It should be noted
+         * that cancelling this event will prevent {@link Post} from being called.
+         */
         public static class Pre extends Held
         {
             public Pre(LivingEntity entity, ItemStack heldItem, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, HandSide handSide, int light, int overlay, float partialTicks)
@@ -103,6 +157,10 @@ public class RenderItemEvent extends Event
             }
         }
 
+        /**
+         * Called after the item has been rendered and if {@link Pre} has not been cancelled. This
+         * event cannot be cancelled.
+         */
         public static class Post extends Held
         {
             public Post(LivingEntity entity, ItemStack heldItem, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, HandSide handSide, int light, int overlay, float partialTicks)
@@ -118,6 +176,10 @@ public class RenderItemEvent extends Event
         }
     }
 
+    /**
+     * This event is fired when rendering an entity item. Use {@link Pre} or {@link Post} when
+     * subscribing to this event.
+     */
     @Cancelable
     public static class Entity extends RenderItemEvent
     {
@@ -129,11 +191,19 @@ public class RenderItemEvent extends Event
             this.entity = entity;
         }
 
+        /**
+         * Gets the item entity being rendered
+         */
         public ItemEntity getEntity()
         {
             return this.entity;
         }
 
+        /**
+         * Called before the item is rendered on the ground. This event can be cancelled to
+         * prevent the item from being rendered and perform any custom rendering. It should be noted
+         * that cancelling this event will prevent {@link Post} from being called.
+         */
         public static class Pre extends Entity
         {
             public Pre(ItemEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
@@ -142,6 +212,10 @@ public class RenderItemEvent extends Event
             }
         }
 
+        /**
+         * Called after the item has been rendered and if {@link Pre} has not been cancelled. This
+         * event cannot be cancelled.
+         */
         public static class Post extends Entity
         {
             public Post(ItemEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
@@ -157,6 +231,10 @@ public class RenderItemEvent extends Event
         }
     }
 
+    /**
+     * This event is fired when rendering an item in an inventory or on the hotbar. Use
+     * {@link Pre} or {@link Post} when subscribing to this event.
+     */
     @Cancelable
     public static class Gui extends RenderItemEvent
     {
@@ -165,6 +243,11 @@ public class RenderItemEvent extends Event
             super(heldItem, ItemCameraTransforms.TransformType.GUI, matrixStack, renderTypeBuffer, light, overlay, Minecraft.getInstance().getRenderPartialTicks());
         }
 
+        /**
+         * Called before the item is rendered in the GUI. This event can be cancelled to prevent the
+         * item from being rendered and perform any custom rendering. It should be noted that
+         * cancelling this event will prevent {@link Post} from being called.
+         */
         public static class Pre extends Gui
         {
             public Pre(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
@@ -173,6 +256,10 @@ public class RenderItemEvent extends Event
             }
         }
 
+        /**
+         * Called after the item has been rendered and if {@link Pre} has not been cancelled. This
+         * event cannot be cancelled.
+         */
         public static class Post extends Gui
         {
             public Post(ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
@@ -188,6 +275,10 @@ public class RenderItemEvent extends Event
         }
     }
 
+    /**
+     * This event is fired when rendering an item on an item frame. Use {@link Pre} or
+     * {@link Post} when subscribing to this event.
+     */
     @Cancelable
     public static class ItemFrame extends RenderItemEvent
     {
@@ -199,11 +290,19 @@ public class RenderItemEvent extends Event
             this.entity = entity;
         }
 
+        /**
+         * Gets the item frame entity that is holding the item
+         */
         public ItemFrameEntity getEntity()
         {
             return this.entity;
         }
 
+        /**
+         * Called before the item is rendered into the item frame. This event can be cancelled to
+         * prevent the item from being rendered and perform any custom rendering. It should be noted
+         * that cancelling this event will prevent {@link Post} from being called.
+         */
         public static class Pre extends ItemFrame
         {
             public Pre(ItemFrameEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
@@ -212,6 +311,10 @@ public class RenderItemEvent extends Event
             }
         }
 
+        /**
+         * Called after the item has been rendered and if {@link Pre} has not been cancelled. This
+         * event cannot be cancelled.
+         */
         public static class Post extends ItemFrame
         {
             public Post(ItemFrameEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
@@ -227,6 +330,11 @@ public class RenderItemEvent extends Event
         }
     }
 
+    /**
+     * This event is fired when rendering an item on a players head. The item that is being rendered
+     * is the one that is currently in the head slot of the player. Use {@link Pre} or {@link Post}
+     * when subscribing to this event.
+     */
     @Cancelable
     public static class Head extends RenderItemEvent
     {
@@ -238,11 +346,19 @@ public class RenderItemEvent extends Event
             this.entity = entity;
         }
 
+        /**
+         * Gets the entity that has the item being rendered to it's head
+         */
         public LivingEntity getEntity()
         {
             return this.entity;
         }
 
+        /**
+         * Called before the item is rendered on the players head. This event can be cancelled to
+         * prevent the item from being rendered and perform any custom rendering. It should be noted
+         * that cancelling this event will prevent {@link Post} from being called.
+         */
         public static class Pre extends Head
         {
             public Pre(LivingEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
@@ -251,6 +367,10 @@ public class RenderItemEvent extends Event
             }
         }
 
+        /**
+         * Called after the item has been rendered and if {@link Pre} has not been cancelled. This
+         * event cannot be cancelled.
+         */
         public static class Post extends Head
         {
             public Post(LivingEntity entity, ItemStack heldItem, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay, float partialTicks)
